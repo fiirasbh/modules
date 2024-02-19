35ef5320d43ef4d58f52b15a2ba8a2793704fda0
fiirasbh <153745191+fiirasbh@users.noreply.github.com>
2024-02-19 11:56:15 +0100

    Update main.tf

--- project/main.tf
+++ project/main.tf
@@ -1,10 +1,10 @@
 module "openstack_network" {
-  source              = "/home/lab/Documents/module_network/project/network_module"
+  source              = "network_module"
   network_name        = "network-test"
   subnet_name         = "subnet-test"
   subnet_cidr         = "172.16.0.0/24"
   router_name         = "router-test"
-  external_network_id = "bcf59eb2-9d83-41cc-b4f5-0435ed594833"
+  external_network_id = ""
   
   external_floating_ip_pool1 = "Ext-Net"
   external_floating_ip_pool2 = "Ext-Net"
